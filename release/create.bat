936346f94e1b88ed6b4d6a0bd4e08e2437334b45
valakiquad <valakiquad@valakiquad-PC>
2013-01-21 14:57:15 +0100

    updated create batch file

--- release/create.bat
+++ release/create.bat
@@ -1,5 +1,5 @@
 del Strategies\Files\jForexBridge.jar
 del Strategies\MultiJForexBridge.java 
 copy ..\target\MultiJForexBridge-1.0.jar Strategies\Files\JForexBridge.jar
-copy ..\src\main\java\multiJForexBridge\MultiJForexBridge.java Strategies\
+copy ..\src\multiJForexBridge\MultiJForexBridge.java Strategies\
 pause
